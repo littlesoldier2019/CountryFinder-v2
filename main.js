0497e254884534bd9823d45a2d016279ed11c8d9
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-01-29 23:34:13 +0200

    resolve case sensitive

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ function countryFinder() {
         for (let i = 0; i < countryDiv.length; i++) {
             countryDiv[i].style.display = 'none';
             
-            if (countryH3[i].innerHTML.startsWith(searchInput.value) === true ) {
+            if (countryH3[i].innerHTML.startsWith(searchInput.value.toUpperCase()) === true ) {
                 countryDiv[i].style.display = 'block';
                 resultNum.push(countryDiv[i]);
             } else {
@@ -150,7 +150,7 @@ function countryFinder() {
         for (let i = 0; i < countryDiv.length; i++) {
             countryDiv[i].style.display = 'none';
 
-            if (countryH3[i].innerHTML.includes(searchInput.value) === true ) {
+            if (countryH3[i].innerHTML.includes(searchInput.valuetoUpperCase()) === true ) {
                 countryDiv[i].style.display = 'block';
                 resultNum.push(countryDiv[i]);
             } else {
